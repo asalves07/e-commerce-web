5e0815de46940043201c763a7772bac5bf5f01f4
André de Souza Alves <alves.andre.ds@gmail.com>
2021-06-25 00:47:03 -0300

    Configure authentication logic

--- pages/Auth/Login/index.tsx
+++ pages/Auth/Login/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import MainComponent from '../../../components/shared/MainComponent';
 import LoginForm from '../../../components/LoginForm';
+import SignUpForm from '../../../components/SignUpForm';
 
 const Login: React.FC = () => {
   return (
     <MainComponent>
       <div className="p-4 text-center">
-        <h2>Entrar</h2>
-        
+        <br />
         <LoginForm titlePhrase="Acessar minha conta" buttonPhrase="ACESSAR" />
+        <br />
+        <SignUpForm titlePhrase="Criar nova conta" buttonPhrase="CRIAR" />
       </div>
     </MainComponent>
   )
